0f9399d3c5c6b39026d11c94cba70cc55cdaebe9
Somasundaram M <msomasundaram93@gmail.com>
2021-05-28 10:23:55 +0530

    style : added colors and fonts

--- app/src/main/java/com/msomu/squareissues/ui/components/IssueItem.kt
+++ app/src/main/java/com/msomu/squareissues/ui/components/IssueItem.kt
@@ -1,31 +1,55 @@
 package com.msomu.squareissues.ui.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
-import androidx.compose.material.Icon
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Warning
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.msomu.squareissues.data.GithubIssuesItem
 import com.msomu.squareissues.mock.mockIssues
 import com.msomu.squareissues.ui.theme.SquareOkhttpIssuesTheme
 
 @Composable
 fun IssueItem(item: GithubIssuesItem) {
-    Card {
+    Card(
+        shape = MaterialTheme.shapes.medium,
+        elevation = 8.dp,
+        backgroundColor = MaterialTheme.colors.background,
+        modifier = Modifier.padding(8.dp)
+    ) {
         Column {
-            Row {
-                Icon(imageVector = Icons.Rounded.Warning, contentDescription = item.state)
-                Text(text = item.title)
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                UserView(item.user, item.updated_at)
+                Status(item.state)
             }
-            Text(text = item.body.take(200))
-            Row {
-                UserView(item.user)
-                Text(text = item.updated_at)
-            }
+            Text(
+                text = item.title,
+                style = MaterialTheme.typography.h3,
+                modifier = Modifier.padding(horizontal = 16.dp)
+            )
+            Text(
+                text = item.body.take(200)+ if(item.body.length > 200) "..." else "",
+                style = MaterialTheme.typography.body2,
+                modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp)
+            )
+            Divider(
+                modifier = Modifier.height(3.dp),
+                color = MaterialTheme.colors.onPrimary
+            )
         }
     }
 }
--- app/src/main/java/com/msomu/squareissues/ui/theme/Type.kt
+++ app/src/main/java/com/msomu/squareissues/ui/theme/Type.kt
@@ -2,27 +2,69 @@ package com.msomu.squareissues.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.msomu.squareissues.R
+
+val Rubik = FontFamily(
+    Font(R.font.rubik_regular),
+    Font(R.font.rubik_light, FontWeight.W300),
+    Font(R.font.rubik_medium, FontWeight.W500),
+    Font(R.font.rubik_bold, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+    h1 = TextStyle(
+        fontFamily = Rubik,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    h2 = TextStyle(
+        fontFamily = Rubik,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp
+    ),
+    h3 = TextStyle(
+        fontFamily = Rubik,
+        fontWeight = FontWeight.W500,
+        fontSize = 16.sp
+    ),
+    h4 = TextStyle(
+        fontFamily = Rubik,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
+    ),
+    body1 = TextStyle(
+        fontFamily = Rubik,
+        fontWeight = FontWeight.Normal,
         fontSize = 14.sp
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
+    body2 = TextStyle(
+        fontFamily = Rubik,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = Rubik,
+        fontWeight = FontWeight.Bold,
+        fontSize = 12.sp
+    ),
+    button = TextStyle(
+        fontFamily = Rubik,
+        fontWeight = FontWeight.W500,
+        fontSize = 18.sp
+    ),
+    h6 = TextStyle(
+        fontFamily = Rubik,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    h5 = TextStyle(
+        fontFamily = Rubik,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp
     )
-    */
 )
